9c7d67b692390a5ccd1fe0df864ac9ac6f30b674
NathanJRollins <NathanRollins@Live.com>
2019-10-18 20:12:47 -0600

    Add octave selection
    Added octaves 1-7 (was only 4).  Greatly improved audio files' quality.  Refactoring.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // TODO:
-//   * Add toggles for which octaves to include
+//   * Add first and last few keys on piano (not complete octaves -
+//       will require rolling random number once, then converting that to octave/note, 
+//       if note probability distribution is to remain even.)
 //   * Display piano keys
-//   * Add toggles for different instruments
+//   * Add toggles for different instruments?
 
 // JS strict mode for habit hygiene (MUST be first statement in file)
 'use strict';
@@ -10,17 +12,20 @@
 var possibleNotesCMajorScale = ["C", "D", "E", "F", "G", "A", "B", ];
 var possibleNotesChromaticScale = ["C", "D", "Db", "E", "Eb", "F", "G", "A", "Ab", "B", "Bb", ];
 var possibleNotes = possibleNotesCMajorScale; // default
+var possibleOctaves = ["4", ];
 var currentAudioElement;
 var state = 'ready-for-new-note'; // functional programming is 4 turds
 
 // initial click is necessary to bypass anti-autoplay protections
 document.onclick = handleClick;
-
 // Handle click by advancing the process as expected for each state
 function handleClick() {
-	// Ensure the functionally-active scale is visually selected (useful for first-run)
-	document.getElementById('scale-cmajor').checked = (possibleNotes == possibleNotesCMajorScale);
-	document.getElementById('scale-chromatic').checked = (possibleNotes == possibleNotesChromaticScale);
+	// if there are no active octaves, tell the user to select some and reset the program state
+	if (possibleOctaves.length == 0) {
+		document.getElementById("display-element").innerText = "👆";
+		state = 'ready-for-new-note';
+		return;
+	}
 
 	// React to click depending on program state
 	switch(state) {
@@ -32,6 +37,9 @@ function handleClick() {
 		case 'ready-for-note-name-display':
 			// display note name & advance program state
 			document.getElementById("display-element").innerText = getNoteNameFromPath(currentAudioElement.src);
+			// include octave name if more than one is selected
+			if (possibleOctaves.length > 1)
+				document.getElementById("display-element").innerText += ' (' + getOctaveNumberFromPath(currentAudioElement.src) + ')';
 			state = 'ready-for-new-note';
 			break;
 		default:
@@ -45,10 +53,18 @@ function handleClick() {
 function playNewNote() {
 	// clear note name display
 	document.getElementById("display-element").innerText = "🎵";
-	// pick a new note at random
-	var newNote = getNewRandomNoteName();
+	// pick new notes and octaves at random, rerolling if both duplicates of the last
+	var lastNote = (currentAudioElement === undefined) ? 'none' : getNoteNameFromPath(currentAudioElement.src);
+	var lastOctave = (currentAudioElement === undefined) ? 'none' : getOctaveNumberFromPath(currentAudioElement.src);
+	var newNote;
+	var newOctave;
+	do {
+		newNote = possibleNotes[Math.floor(Math.random()*possibleNotes.length)];
+		newOctave = possibleOctaves[Math.floor(Math.random()*possibleOctaves.length)];
+	}
+	while ((newNote == lastNote) && (newOctave == lastOctave));
 	// get its corresponding audio element
-	currentAudioElement = getAudioElementFor(newNote);
+	currentAudioElement = getAudioElementFor(newOctave, newNote);
 	// play it from the beginning
 	currentAudioElement.currentTime = 0.0;
 	currentAudioElement.play();
@@ -56,29 +72,23 @@ function playNewNote() {
 	state = 'ready-for-note-name-display';
 }
 
-// Get the note's name cleanly, without path or extension
+// Get the note's name cleanly, from the given path, without its extension
 function getNoteNameFromPath(path) {
-	var name = path.slice(path.lastIndexOf("/")+1, path.lastIndexOf("."));
-	name = name;
-	return name;
+	// return path.slice(path.lastIndexOf("/")+1, path.lastIndexOf("."));
+	var filename = path.slice(path.lastIndexOf("/")+1, path.lastIndexOf("."));
+	var note = filename.slice(filename.lastIndexOf("-")+1);
+	return note;
 }
 
-// Set a new (random) note up to be played
-function getNewRandomNoteName() {
-	// determine which note was played last so we don't repeat it
-	var lastNote = (currentAudioElement === undefined) ? 'none' : getNoteNameFromPath(currentAudioElement.src) ;
-
-	// get new note at random from possibleNotes array (no dupes)
-	var newNote;
-	do {
-		newNote = possibleNotes[Math.floor(Math.random()*possibleNotes.length)];
-	}
-	while (newNote == lastNote); // no dupes
-	return newNote;
+// Get the octave's number cleanly, from the given path
+function getOctaveNumberFromPath(path) {
+	var filename = path.slice(path.lastIndexOf("/")+1, path.lastIndexOf("."));
+	var octave = filename.slice(0,filename.lastIndexOf("-"));
+	return octave;
 }
 
 // Return an audio element for the given note, whether one already exists or not
-function getAudioElementFor(note) {
+function getAudioElementFor(octave, note) {
 	// get the element if it already exists
 	var thisAudioElement = document.getElementById('audioElementForNote-'+note);
 	// if the element does not already exist, create it, attach it, and return it
@@ -86,7 +96,7 @@ function getAudioElementFor(note) {
 		// create and initialize new audio element for this note 
 		thisAudioElement = document.createElement('audio');
 		thisAudioElement.setAttribute('id', 'audioElementForNote-'+note);
-		thisAudioElement.setAttribute('src', `audio/note/piano/octave-4/${note}.mp3`);
+		thisAudioElement.setAttribute('src', `audio/note/piano/${octave}-${note}.mp3`);
 		thisAudioElement.load();
 	}
 	return thisAudioElement;
@@ -98,3 +108,30 @@ function scaleChanged(newScale) {
 	if (newScale == "cmajor")
 		possibleNotes = possibleNotesCMajorScale;
 }
+
+// Add/remove octave from list of possible octaves
+function updateSelectedOctave(event) {
+	// if it's selected & not in the list of possibilities, add it
+	var caller = event.target;
+	var octaveNumber = caller.id[caller.id.length-1];
+	if (caller.checked && !possibleOctaves.includes(octaveNumber))
+		possibleOctaves.push(octaveNumber);
+	// if it's not selected and it's in the list, remove it
+	else if (possibleOctaves.includes(octaveNumber))
+		possibleOctaves.splice(possibleOctaves.indexOf(octaveNumber), 1);
+
+	// if no possible octaves remain, handle that
+	if(possibleOctaves.length <= 0)
+		handleClick();
+	
+	event.stopPropagation(); // kill the click event here
+}
+// Attach this handler to the elements (we need the event, to stop propagation,
+//   so it cannot be assigned inline in the HTML's "onclick="")
+document.getElementById("octave-1").addEventListener("click", updateSelectedOctave);
+document.getElementById("octave-2").addEventListener("click", updateSelectedOctave);
+document.getElementById("octave-3").addEventListener("click", updateSelectedOctave);
+document.getElementById("octave-4").addEventListener("click", updateSelectedOctave);
+document.getElementById("octave-5").addEventListener("click", updateSelectedOctave);
+document.getElementById("octave-6").addEventListener("click", updateSelectedOctave);
+document.getElementById("octave-7").addEventListener("click", updateSelectedOctave);
